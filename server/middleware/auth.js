8f4ca2f7bef34f165f595d1900464b82d0e0c158
Shruti <shruti2904h@gmail.com>
2026-07-11 20:19:09 +0530

    ongoing chat app

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+//to confirm user authentication, middleware to protect routes i.e. user is only routed if authenticated
+
+export const protectRoute = async (req, res, next)=>{
+    try {
+        const token = req.headers.token;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user) return res.json({success: false, message: "User not found" });
+
+        req.user = user;
+        next(); //executes the controller function
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message });
+    }
+}
