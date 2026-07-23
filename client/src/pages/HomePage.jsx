79f7123223feb27cd0998c4f21fd6b97ce220cce
Shruti <shruti2904h@gmail.com>
2026-07-23 22:32:04 +0530

    fix bugs

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import RightSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
 
-   const [selectedUser, setSelectedUser] = useState(null) //tut had useStae(false)
+   const [selectedUser, setSelectedUser] = useState(null) //useStae(false)?
 
   return (
     <div className = 'border w-full h-screen sm:px-[15%] sm:py-[5%]'>
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -46,7 +46,7 @@ const ProfilePage = () => {
 
            <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
         </form>
-        <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`} src={assets.logo_icon} alt="" />
+        <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`} src={authUser?.profilePic || assets.logo_icon} alt="" />
       </div>
 
     </div>
