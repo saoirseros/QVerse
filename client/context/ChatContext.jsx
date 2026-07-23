376b6c15571f4ed8fe0721c226a9979c15850d10
Shruti <shruti2904h@gmail.com>
2026-07-23 20:36:17 +0530

    add chat context

---
+++ client/context/ChatContext.jsx
@@ -0,0 +1,89 @@
+import { useContext } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({ children })=>{
+
+    const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [unseenMessages, setUnseenMessages] = useState({});
+
+    const {socket, axios} = useContext(AuthContext);
+
+    //function to get all users for sidebar
+    const getUsers = async () =>{
+        try {
+            const { data } = await axios.get("/api/messages/users");
+            if (data.success){
+                setUsers(data.users)
+                setUnseenMessages(data.unseenMessages)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+    //fucntion to get messages for selected user
+    const getMessages = async (userId)=>{
+        try {
+           const { data } = await axios.get(`/api/messages/${userId}`);
+           if (data.success){
+            setMessages(data.messages)
+           }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    //function to send message to selected user
+
+    const sendMessage = async (messageData)=>{
+        try {
+            const {data} = await axios.post(`/api/messages/send/${selectedUser._id}`, messageData);
+
+            if(data.success){
+                setMessages((prevMessages)=>[...prevMessages, data.newMessage])
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    //function to subscribe to messages for selcted user - meaning we'll get the new msgs in real time instantly
+
+    const subscribeToMessages = async ()=>{
+        if(!socket) return;
+
+        socket.on("newMessage", (newMessage)=>{
+            if(selectedUser && newMessage.senderId === selectedUser._id){
+                newMessage.seen = true;
+                setMessages((prevMessages)=> [...prevMessages, newMessage]);
+
+                axios.put(`/api/messages/mark/${newMessage._id}`);
+            }else{
+                setUnseenMessages((prevUnseenMessages)=>({
+                    ...prevUnseenMessages, [newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+                }))
+            }
+        })
+    }
+
+
+    const value = {
+        
+    }
+
+    return( 
+    <ChatContext.Provider value={value}>
+        { children }
+    </ChatContext.Provider>
+    )
+}
